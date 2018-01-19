ef025866c736166b12e6c8bcad6b081f6f5dc98c
Max <chestm007@hotmail.com>
2018-01-19 20:17:35 +1100

    fix setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
+required_packages = [
+    'RPi.GPIO'
+]
+
 setup(
     name='py_hydropi',
-    version='',
-    packages=['py_hydropi', 'py_hydropi.lib'],
-    url='',
-    license='',
-    author='max',
-    author_email='',
-    description=''
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='',
+    install_requires=required_packages,
+    entry_points="""
+        [console_scripts]
+        py_hydropi=py_hydropi.main:main
+    """
 )
