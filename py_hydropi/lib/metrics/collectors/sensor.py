a76eb04f54c6ed0617917a54614d7367d426a291
Max <chestm007@hotmail.com>
2018-06-29 13:50:25 +1000

    dont report input = 0 (this may turn out to be a regression later)

--- py_hydropi/lib/metrics/collectors/sensor.py
+++ py_hydropi/lib/metrics/collectors/sensor.py
@@ -5,4 +5,4 @@ class SensorMetricCollector:
         self.reporter = reporter
 
     def push_all(self):
-        return [(id, sensor.value) for id, sensor in self.db._inputs.items()]
+        return [(id, sensor.value or None) for id, sensor in self.db._inputs.items()]
