f637da50d4ed3a4b82604421d8df866fc191b3b8
Max Chesterfield <chestm007@hotmail.com>
2018-11-11 15:32:44 +1100

    Beginnings of CI

--- setup.py
+++ setup.py
@@ -2,14 +2,7 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-required_packages = [
-    'RPi.GPIO==0.6.3',
-    'PyYAML==3.12',
-    'cherrypy==13.1.0',
-    'Adafruit_Python_DHT==1.3.2',
-    'systemd',
-    'hcsr04sensor',
-]
+required_packages = []
 
 setup(
     name='py_hydropi',
