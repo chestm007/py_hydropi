1ce4367086cac4ee7c1d9308e20b2377ad11d709
Max Chesterfield <chestm007@hotmail.com>
2018-11-11 15:54:23 +1100

    ify setup.py to install raspberrypi deps when needed

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from distutils.core import setup
+import os
 
 from setuptools import find_packages
 
-required_packages = []
+required_packages = [
+    'PyYAML>=3.12',
+    'cherrypy>=13.1.0',
+    'systemd',
+    'pep8',
+    'requests'
+]
+
+is_rasp_pi = os.uname()[4].startswith('arm')
+if is_rasp_pi:
+    required_packages.extend([
+        'RPi.GPIO',
+        'Adafruit_Python_DHT',
+        'hcsr04sensor'
+    ])
 
 setup(
     name='py_hydropi',
