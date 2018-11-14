3fea0e14c5eb8b1fabae90e28feed482be5d7212
chestm007 <chestm007@hotmail.com>
2018-11-14 22:55:59 +1100

    speed up tests by waiting less for simpletimer

--- tests/module_tests/simple_timer_test.py
+++ tests/module_tests/simple_timer_test.py
@@ -2,7 +2,6 @@ import time
 
 from py_hydropi.lib.modules.timer import SimpleTimer
 from module_tests.base_test_object import BaseTestObject
-_multiprocess_can_split_ = True
 
 
 class TestSimpleTimer(BaseTestObject):
@@ -11,20 +10,20 @@ class TestSimpleTimer(BaseTestObject):
     @classmethod
     def setUpClass(cls):
         cls.output = cls.MockOutput(1)
-        cls.simpletimer = SimpleTimer(on_time='1s', off_time='1s')
-        cls.simpletimer.attach_object(cls.output)
-        cls.simpletimer.start()
+        cls.simpletimer = SimpleTimer(on_time='0.1s', off_time='0.1s')\
+            .attach_object(cls.output)\
+            .start()
         time.sleep(0.2)
 
     def test_intervals(self):
         if self.output.active:
-            time.sleep(1)
+            time.sleep(0.1)
 
         for i in range(2):
             self.assertFalse(self.output.active)
-            time.sleep(1)
+            time.sleep(0.1)
             self.assertTrue(self.output.active)
-            time.sleep(1)
+            time.sleep(0.1)
 
     @classmethod
     def tearDownClass(cls):
